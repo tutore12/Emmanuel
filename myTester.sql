aea5f33d70fd08a32549ebc4c5c192f911116e8c
tutorforyouth2020@outlook.com <tutorforyouth2020@outlook.com>
2021-08-26 22:08:52 +0000

    this my first submmit

---
+++ myTester.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC ### This the first book to test the connection 
+
+-- COMMAND ----------
+
+select * from default.students
+
+-- COMMAND ----------
+
+
